0fd17e294f4cca8d8668f6bb0ef87f5ed33d3d48
Shav <shavenchew@yahoo.com>
2016-11-28 13:42:04 -0800

    Basic Index Route with hard coded single data

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -14,16 +15,52 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//mongoose database setup
+mongoose.connect('mongodb://localhost/coltBlogs');
+
+var blogSchema = new mongoose.Schema({
+  title : String,
+  image : String,
+  description: String
+});
+
+var Blogs = mongoose.model('blog', blogSchema);
+
+Blogs.create({
+  title: 'My First Post',
+  image : 'https://source.unsplash.com/random',
+  description : 'Trying out how this works'
+}, function(err, result){
+  if(err){
+    console.log(err);
+  } else {
+    console.log('Sucess');
+    console.log(result);
+  }
+});
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
 
-app.use('/', index);
-app.use('/users', users);
+// app.use('/', index);
+// app.use('/users', users);
+
+
+app.get('/', function(req, res){
+  Blogs.find({}, function(err, blogs){
+    if(err){
+      console.log(err);
+    } else {
+        res.render('index', {blogs: blogs});
+    }
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,4 +80,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+// module.exports = router;
+
+app.listen('8000', function(){
+  console.log('Blog Now Running!');
+});
