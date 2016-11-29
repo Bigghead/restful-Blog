a229d5780a7b2ccef1d23a5a850e1a327bc8ba80
Shav <shavenchew@yahoo.com>
2016-11-28 17:03:56 -0800

    Working on an EDIT Route. First time trying it

--- app.js
+++ app.js
@@ -110,6 +110,13 @@ app.post('/blogs', function(req, res){
   });
 });
 
+
+//EDIT ROUTE
+app.get('/blogs/:id/edit', function(req, res){
+  res.render('edit');
+});
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
